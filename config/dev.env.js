479afca884acdaba75de3f2ce2f2a2f6ead4bb3b
wkeyuan <keyuan258@gmail.com>
2022-01-21 12:30:36 +0800

    添加配置说明

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
+  // 本地开发环境后端API地址不需要在这个文件配置，在config/index.js里面配置target
   DW_API_URL: '"http://localhost:8081"',
   DW_WEB_URL: '"http://localhost:8081"',
   DW_RESOURCE_URL: '"http://localhost:8080"'
