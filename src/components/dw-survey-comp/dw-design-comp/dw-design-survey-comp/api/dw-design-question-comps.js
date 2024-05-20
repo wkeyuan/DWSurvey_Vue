4357ddf8bc29fbdcf56d401545c6e87c56fe6f0a
CodeSky <service@diaowen.net>
2024-05-20 23:04:18 +0800

    同步后端地址统一加上/v6

--- src/components/dw-survey-comp/dw-design-comp/dw-design-survey-comp/api/dw-design-question-comps.js
+++ src/components/dw-survey-comp/dw-design-comp/dw-design-survey-comp/api/dw-design-question-comps.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function bankQuestions () {
   return request({
-    url: '/api/dwsurvey/app/dw-design-survey/bank-qus.do',
+    url: '/api/dwsurvey/app/v6/dw-design-survey/bank-qus.do',
     method: 'get'
   })
 }
